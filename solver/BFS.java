adbb122f4708c9887b56bf39dc7bbdb6ac95b5b9
Keyur <keyur2002solanki@gmail.com>
2021-01-13 22:29:45 +0530

    Implemented Breadth First Search

---
+++ solver/BFS.java
@@ -0,0 +1,51 @@
+package solver;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import maze.Maze;
+
+public class BFS extends MazeSolver {
+    Queue<Cell> cellQueue;
+
+    BFS(int size) {
+        this(size, size);
+    }
+
+    BFS(int row, int col) {
+        super(row, col);
+        cellQueue = new LinkedList<>();
+        cellQueue.add(current);
+    }
+
+    public Cell nextIteration() {
+        Cell last  = current;
+        if (last != start && last != dest)
+            last.color = VISITED_COLOR;
+
+        if (current == dest) {
+            finished = true;
+            return null;
+        }
+
+        for (Cell cell : current.neighbors) {
+            cellQueue.add(cell);
+            cell.neighbors.remove(current);
+            cell.last = current;
+        }
+        current.neighbors.clear();
+        current = cellQueue.remove();
+        if (current != start && current != dest)
+            current.color = Maze.CURR_CELL_COLOR;
+
+        return last;
+    }
+
+    public void pathFound() {
+        current = current.last;                             // Popping the destintion node
+        while (current.last != null) {
+            current.color = Maze.PATH_COLOR; // Popping each node in path and changing its color
+            current = current.last;
+        }
+    }
+}
