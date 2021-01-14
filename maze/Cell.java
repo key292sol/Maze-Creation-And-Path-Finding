5d1fa3898f78caf5550d284fd2d3d0ec4fbccbd8
Keyur <keyur2002solanki@gmail.com>
2021-01-14 16:50:10 +0530

    Removed Cell class from Maze class

---
+++ maze/Cell.java
@@ -0,0 +1,18 @@
+package maze;
+
+import java.util.ArrayList;
+import java.awt.Color;
+
+public class Cell {
+    public boolean visited = false;
+    public Color color = Maze.NOT_VISITED_COLOR;
+    public boolean[] walls = { true, true, true, true }; // up right down left
+    public int row, col;
+    public ArrayList<Cell> neighbors = new ArrayList<>();
+    public Cell last;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
