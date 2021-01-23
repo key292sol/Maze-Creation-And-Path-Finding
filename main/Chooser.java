e27b84bbe1c4f4d04cfaf0e5e92672a9776ad3b6
Keyur <keyur2002solanki@gmail.com>
2021-01-23 22:44:01 +0530

    You can now choose the options you want to watch

--- main/Chooser.java
+++ main/Chooser.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -11,6 +13,8 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
 public class Chooser extends JFrame {
+    private static final long serialVersionUID = 1L;
+
     JSpinner rowSpinner, colSpinner, delaySpinner;
     JButton startGenBut, startFindBut, initBut;
     JComboBox<String> genChose, findChose;
@@ -22,6 +26,8 @@ public class Chooser extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(6, 3, 5, 5));
         setSize(400, 250);
+
+        MyActionListener mal = new MyActionListener();
 
         rowSpinner = new JSpinner(new SpinnerNumberModel(10, 5, 150, 1));
         colSpinner = new JSpinner(new SpinnerNumberModel(10, 5, 150, 1));
@@ -37,6 +43,13 @@ public class Chooser extends JFrame {
         initGenChose();
         initFindChose();
 
+        startGenBut.setEnabled(false);
+        startFindBut.setEnabled(false);
+
+        initBut.addActionListener(mal);
+        startGenBut.addActionListener(mal);
+        startFindBut.addActionListener(mal);
+
         add(new JLabel("Rows"));        add(rowSpinner);    add(new JLabel());
         add(new JLabel("Columns"));     add(colSpinner);    add(new JLabel());
         add(new JLabel("Generator"));   add(genChose);      add(genCheck);
@@ -45,7 +58,7 @@ public class Chooser extends JFrame {
         add(initBut);                   add(startGenBut);   add(startFindBut);
 
         setVisible(true);
-        df = new DisplayFrame(this);
+        df = getNewDisplayFrame();
     }
 
     public void initGenChose() {
@@ -61,6 +74,34 @@ public class Chooser extends JFrame {
         String vals[] = {"DFS", "BFS", "A*"};
         for (String str : vals) {
             findChose.addItem(str);
+        }
+    }
+
+    private DisplayFrame getNewDisplayFrame(){
+        return new DisplayFrame(this);
+    }
+
+    public void genComplete() {
+        startGenBut.setEnabled(false);
+        startFindBut.setEnabled(true);
+    }
+
+    class MyActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent ae) {
+            Object src = ae.getSource();
+            if (src == initBut) {
+                startGenBut.setEnabled(true);
+                if (df == null) df = getNewDisplayFrame();
+                df.init();
+            } else if (src == startGenBut) {
+                // Start Maze Generation
+                startGenBut.setEnabled(false);
+                df.startGeneration();
+            } else {
+                // Start Maze Solving
+                startFindBut.setEnabled(false);
+                df.startSolving();
+            }
         }
     }
 }
--- main/DisplayFrame.java
+++ main/DisplayFrame.java
@@ -1,18 +1,152 @@
 package main;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
+import generator.*;
+import solver.*;
+import maze.*;
+
 public class DisplayFrame extends JFrame {
+    private static final long serialVersionUID = 1L;
+
     Chooser ch;
+
+    private int rows, cols, delay;
+    private boolean visualizeGen, visualizeFind;
+    private boolean genStarted, genEnded;
+    private boolean findStarted, findEnded;
+
+    private MazeGenerator generator;
+    private MazeSolver solver;
+    private Maze mazeObj;
 
     public DisplayFrame(Chooser ch) {
         this.ch = ch;
-        setSize(700, 700);
+        setSize(Maze.frameWidth + 25, Maze.frameHeight + 50);
+        setLocationRelativeTo(null);
         setVisible(true);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void init() {
-        
+        rows = (Integer)ch.rowSpinner.getValue();
+        cols = (Integer)ch.colSpinner.getValue();
+        double d = (Double)ch.delaySpinner.getValue();
+        delay = (int) d * 1000;
+        visualizeGen = ch.genCheck.isSelected();
+        visualizeFind = ch.findCheck.isSelected();
+
+        genStarted = false;
+        genEnded = false;
+        findStarted = false;
+        findEnded = false;
+
+        switch (ch.genChose.getSelectedIndex()) {
+            case 0: 
+                generator = new RecursiveBacktrackingAlgorithm(rows, cols);
+                break;
+            case 1: 
+                generator = new RandomPrims(rows, cols);
+                break;
+            default:
+                System.out.println("How a default generator ?");
+                return;
+        }
+
+        switch (ch.findChose.getSelectedIndex()) {
+            case 0:
+                solver = new DepthFirstSearch();
+                break;
+            case 1:
+                solver = new BFS();
+                break;
+            case 2:
+                solver = new AStar();
+                break;
+        }
+
+        mazeObj = generator;
+        repaint();
+    }
+
+    public void startGeneration() {
+        genStarted = true;
+        repaint();
+    }
+
+    public void startSolving() {
+        findStarted = true;
+        repaint();
+    }
+
+    private void endGen() {
+        ch.genComplete();
+        solver.setGenerator(generator);
+        mazeObj = solver;
+    }
+
+    public void paint(Graphics g) {
+        if (mazeObj == null) return;
+        if (!genStarted) {
+            super.paint(g);
+            drawWholeGrid(g);
+            return;
+        }
+
+        if (!genEnded) {
+            if (!visualizeGen) {
+                generator.completeAllIterations();
+                endGen();
+                genEnded = true;
+                drawWholeGrid(g);
+                return;
+            }
+            Cell cell = generator.nextIteration();
+            if (generator.current != null && cell != null) {
+                generator.drawCell(g, cell);
+                generator.drawCell(g, generator.current);
+            } else {
+                endGen();
+                genEnded = true;
+                drawWholeGrid(g);
+            }
+        } else if (findStarted && !findEnded) {
+            if (visualizeFind) {
+                if (!solver.finished) {
+                    g.setColor(Maze.VISITED_COLOR);
+                    solver.drawCell(g, solver.current);
+                    Cell last = solver.nextIteration();
+                    if (last != null) {
+                        solver.drawCell(g, last);
+                        solver.drawCell(g, solver.current);
+                    }
+                } else {
+                    solver.lastPathCell();
+                    solver.drawCell(g, solver.current);
+                }
+            } else {
+                solver.completeAllIterations();
+                ch.startFindBut.setEnabled(false);
+                drawWholeGrid(g);
+                findEnded = true;
+            }
+        }
+
+        if (!findEnded) repaint();
+
+        try {
+            Thread.sleep(delay);
+        } catch (Exception e) {
+        }
+    }
+
+    public void drawWholeGrid(Graphics g) {
+        for (Cell[] cells: mazeObj.maze) {
+            for (Cell cell : cells) {
+                mazeObj.drawCell(g, cell);
+            }
+        }
     }
 }
