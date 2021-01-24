acd4a4088fc1140774563d16651e1e10916af042
Keyur <keyur2002solanki@gmail.com>
2021-01-24 14:39:06 +0530

    Moved setInititalNeighbors(), getRandomNeighbor() and setNeighbors() to MazeGenerator.java

--- generator/MazeGenerator.java
+++ generator/MazeGenerator.java
@@ -2,23 +2,22 @@ package generator;
 
 import maze.*;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public abstract class MazeGenerator extends Maze {
     Random random = new Random();
 
-    MazeGenerator(int gridSize) {
+    public MazeGenerator(int gridSize) {
         this(gridSize, gridSize);
     }
 
-    MazeGenerator(int row, int col) {
+    public MazeGenerator(int row, int col) {
         // The blockSize shouldn't be less then 5
-        int maxLength = Math.min(Maze.frameHeight, Maze.frameWidth) / 5;
-        if (Math.max(row, col) < maxLength)
-            blockSize = Math.min(Maze.frameHeight, Maze.frameWidth) / Math.max(row, col);
-        else
-            blockSize = 5;
+        blockSize = Math.min(Maze.frameHeight, Maze.frameWidth) / Math.max(row, col);
+        blockSize = (blockSize < 5) ? 5 : blockSize;
 
+        // Initializing the maze
         maze = new Cell[row][col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -26,6 +25,7 @@ public abstract class MazeGenerator extends Maze {
             }
         }
 
+        // Setting neighbors
         for (Cell[] cells : maze) {
             for (Cell cell : cells) {
                 setInitialNeighbors(cell);
@@ -35,12 +35,69 @@ public abstract class MazeGenerator extends Maze {
         current = getRandomCell(row, col);
     }
 
+    /*
+     * For initilizing neighbors to all the cells in its neighbors
+     * without checking if they have a wall between them or not.
+     * Used to set neighbors before maze generation
+     * @param   cell    The cell whose neighbors are to be initialized
+     */
+    public void setInitialNeighbors(Cell cell) {
+        Cell[] potentials = { getCellAt(cell.row - 1, cell.col), getCellAt(cell.row, cell.col + 1), getCellAt(cell.row + 1, cell.col),
+            getCellAt(cell.row, cell.col - 1), };
+        for (Cell n : potentials) {
+            if (n != null)
+                cell.neighbors.add(n);
+        }
+    }
+    
+    // @param   cell    The cell whose neighbor is to be returned
+    // @return  Cell    A random neighbor or null if no neighbors are left unvisited
+    public Cell getRandomNeighbor(Cell cell) {
+        // For checking which neighbors have been visited
+        boolean visitedN[] = new boolean[cell.neighbors.size()];
+        int i = 0;
+        while (i < cell.neighbors.size()) {
+            visitedN[i] = cell.neighbors.get(i).visited;
+            i++;
+        }
+
+        // If a neighbor has been visited then remove it
+        // We don't need to go to alreaady visited neighbors
+        while (i > 0) {
+            i--;
+            if (visitedN[i]) {
+                cell.neighbors.remove(i);
+            }
+        }
+
+        // If no neighbors left return null
+        if (cell.neighbors.size() == 0)
+            return null;
+        return cell.neighbors.remove(random.nextInt(cell.neighbors.size()));
+    }
+
+    // Used to set neighbors after completing the generation
+    // @param   cell    The cell whose neighbors are to be initialized
+    public void setNeighbors(Cell cell) {
+        cell.neighbors = new ArrayList<>();
+        Cell[] potentials = { getCellAt(cell.row - 1, cell.col), getCellAt(cell.row, cell.col + 1), getCellAt(cell.row + 1, cell.col),
+                getCellAt(cell.row, cell.col - 1), };
+
+        // If there exists no wall between the 2 cells then add them as neighbors
+        for (int i = 0; i < potentials.length; i++) {
+            if (!cell.walls[i]) {
+                cell.neighbors.add(potentials[i]);
+            }
+        }
+    }
+
     @Override
     public void completeAllIterations() {
         super.completeAllIterations();
         setGridNotVisited();
     }
 
+    // Setting maze to not visited for solving the maze afterwards
     public void setGridNotVisited() {
         current = null;
         for (Cell[] cells : maze) {
@@ -53,9 +110,10 @@ public abstract class MazeGenerator extends Maze {
         }
     }
 
+    // Remove wall between current and the passed Cell
+    // Checking order acc to this.current cell: Top Right Bottom Left
+    // @param   to  The neighbor of current cell
     public void removeWall(Cell to) {
-        // Checking which wall to remove
-        // OrdeChecking order acc to this.current cell: Top Right Bottom Left
         if (current.row > to.row) {
             current.walls[0] = false;
             to.walls[2]      = false;
--- maze/Maze.java
+++ maze/Maze.java
@@ -1,6 +1,5 @@
 package maze;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.awt.Graphics;
 import java.awt.Color;
@@ -19,49 +18,13 @@ public abstract class Maze {
     public Cell current;
     public int blockSize;
 
+    // For performing a new iteration of solver or generator
+    // Must return null on completion
     public abstract Cell nextIteration();
 
+    // If you don't want to visualize
     public void completeAllIterations() {
         while (nextIteration() != null) {}
-    }
-    
-    public void setInitialNeighbors(Cell cell) {
-        Cell[] potentials = { getCellAt(cell.row - 1, cell.col), getCellAt(cell.row, cell.col + 1), getCellAt(cell.row + 1, cell.col),
-            getCellAt(cell.row, cell.col - 1), };
-        for (Cell n : potentials) {
-            if (n != null)
-                cell.neighbors.add(n);
-        }
-    }
-
-    public void setNeighbors(Cell cell) {
-        cell.neighbors = new ArrayList<>();
-        Cell[] potentials = { getCellAt(cell.row - 1, cell.col), getCellAt(cell.row, cell.col + 1), getCellAt(cell.row + 1, cell.col),
-                getCellAt(cell.row, cell.col - 1), };
-        for (int i = 0; i < potentials.length; i++) {
-            if (!cell.walls[i]) {
-                cell.neighbors.add(potentials[i]);
-            }
-        }
-    }
-        
-    public Cell getRandomNeighbor(Cell cell) {
-        Random random = new Random();
-        boolean visitedN[] = new boolean[cell.neighbors.size()];
-        int i = 0;
-        while (i < cell.neighbors.size()) {
-            visitedN[i] = cell.neighbors.get(i).visited;
-            i++;
-        }
-        while (i > 0) {
-            i--;
-            if (visitedN[i]) {
-                cell.neighbors.remove(i);
-            }
-        }
-        if (cell.neighbors.size() == 0)
-            return null;
-        return cell.neighbors.remove(random.nextInt(cell.neighbors.size()));
     }
 
     public Cell getCellAt(int r, int c) {
@@ -75,17 +38,26 @@ public abstract class Maze {
         return getCellAt(random.nextInt(rows), random.nextInt(cols));
     }
 
+    /*
+     * @param   g       The Graphics object on which you need to draw cell
+     * @param   cell    The cell needed to be drawn
+     */
     public void drawCell(Graphics g, Cell cell) {
+        // Draw the rectangle/square shape of the cell
         g.setColor(cell.color);
         g.fillRect(getDrawXPos(cell.col) + 1, getDrawYPos(cell.row) + 1, blockSize, blockSize);
-        g.setColor(Color.BLACK);
 
+        // Draw walls
+        g.setColor(Color.BLACK);
         int[][] walls = {                                                               // x1, y1, x2, y2 positions
                 { (cell.col),     (cell.row),     (cell.col + 1), (cell.row)     },     // top wall
                 { (cell.col + 1), (cell.row),     (cell.col + 1), (cell.row + 1) },     // right wall
                 { (cell.col),     (cell.row + 1), (cell.col + 1), (cell.row + 1) },     // bottom wall
                 { (cell.col),     (cell.row),     (cell.col),     (cell.row + 1) }      // left wall
         };
+
+        // If there exists a wall there, then draw a line
+        // representing the wall
         for (int i = 0; i < walls.length; i++) {
             if (cell.walls[i]) {
                 g.drawLine(getDrawXPos(walls[i][0]), getDrawYPos(walls[i][1]), getDrawXPos(walls[i][2]),
@@ -94,10 +66,14 @@ public abstract class Maze {
         }
     }
 
+    // @param   x   X position of the point
+    // @return  int X position of where to draw
     private int getDrawXPos(int x) {
         return x * blockSize + 10; // +10 because of Frame borders
     }
 
+    // @param   y   Y position of the point
+    // @return  int Y position of where to draw
     private int getDrawYPos(int y) {
         return y * blockSize + 35; // +35 because of Frame borders
     }
